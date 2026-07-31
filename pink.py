1d7ac63c7ce4b4293ab1466374c4dd24bd59a996
Max V. <mountainwater101z@gmail.com>
2026-07-31 12:42:19 -0500

    Implement multiple endings based on trust level

--- pink.py
+++ pink.py
@@ -1,1 +1,12 @@
+# MULTIPLE ENDINGS 
 
+trust = 7 
+saved_friend = True 
+
+if trust => 5 and saved_friend:
+  print (" GOOD ENDING")
+elif trust < 0: 
+  print("BAD ENDING")
+else:
+  print("NEUTRAL ENDING")
+  
