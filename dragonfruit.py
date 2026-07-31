1caaffb13047fb6ec8611ad3e59931e598d746d5
Max V. <mountainwater101z@gmail.com>
2026-07-30 19:36:09 -0500

    Add choice branching system in dragonfruit.py

--- dragonfruit.py
+++ dragonfruit.py
@@ -1,1 +1,11 @@
+#Choice + Branching System
 
+choice = input("Go to the library or roofttop?").lower()
+
+if choice == "Library":
+  print("You find a mysterious notebook.")
+elif choice == "Rooftop":
+  print("Someone is waiting for you.")
+else:
+  print("You stay where you are.")
+  
