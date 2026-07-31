763629892ca09db0ac53b5349000d1777c529f64
Max V. <mountainwater101z@gmail.com>
2026-07-30 19:42:11 -0500

    Implement basic affection system in woodenchair.py

--- woodenchair.py
+++ woodenchair.py
@@ -1,1 +1,17 @@
+# RELATIONSHIP/AFFECTION SYSTEM
 
+affection = 0
+
+choice = input("Compliment them? yes/no: ")
+
+if choice == "yes":
+    affection += 2
+else:
+  affection -= 1
+print("Affection: ", affection)
+
+
+if affection >=5:
+  print("Special bonding scene unlocked!")
+  
+  
