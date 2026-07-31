6aa790ddc62891cfce7d4cefe0667c66695ea4e5
Max V. <mountainwater101z@gmail.com>
2026-07-30 20:17:07 -0500

    Implement story memory flags for player actions
    Added flags to track player actions and updated logic to check if a letter was read.

--- blue_pie.py
+++ blue_pie.py
@@ -1,1 +1,14 @@
+# FLAGS/STORY MEMORY 
+#--Remembering what the player has done--
 
+flags = {
+  "met_Batman": False,
+  "read_letter": False,
+  "saved_friend": False
+}
+flags["read_letter"] = True 
+
+if flags ["read_letter"]: 
+  print("You recognize the handwriting.")
+else: 
+    print("The handwriting means nothing to you.")
