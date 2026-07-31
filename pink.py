e7ea83cb951eef3602cb0139b37211d44ec4524c
Max V. <mountainwater101z@gmail.com>
2026-07-31 12:53:38 -0500

    Add final night prompts and trust level input

--- pink.py
+++ pink.py
@@ -9,4 +9,15 @@ elif trust < 0:
   print("BAD ENDING")
 else:
   print("NEUTRAL ENDING")
-  
+
+print("xo"*10)
+
+print("xoxo ZOMBIE APOCALYPSE XOXO")
+print("You have reached the final night.\.n")
+
+trust = int(input("What is your group's trust level (0-10)?"))
+saved_friend = input("Did you save your friend? (yes/no): ").lower() == "yes"
+found_cure = input("Did you find the Zombie cure? (yes/no): ").lower() == "yes"
+romance = int(input("What is your romance level (0-10)?:))
+
+
