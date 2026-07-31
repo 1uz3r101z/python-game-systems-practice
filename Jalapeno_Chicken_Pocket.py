168962214455c9ab928469a452b4f87e61d6db89
Max V. <mountainwater101z@gmail.com>
2026-07-30 19:25:02 -0500

    Correct print statement syntax and dialogue
    Fix syntax errors in print statements and improve dialogue clarity.

--- Jalapeno_Chicken_Pocket.py
+++ Jalapeno_Chicken_Pocket.py
@@ -6,6 +6,6 @@ print(f"{name}...y-you actually came...")
 
 reply = input("1. Of course I came. Why wouldn't I?\n2. I almost didn't make it...I wasn't sure if it would have been the right move.\n>")
 if reply == "1":
-    print(f"{name} I don't know I knew you wanted me to come but at the same time I wasn't going to assume. Regardless I'm glad you made it."
+    print(f"{name} I don't know...I knew you wanted me to come but at the same time I wasn't going to assume. Regardless I'm glad you made it."
 else:
     print:(f" Well {name} I'm glad you showed up regardless of your doubts... I hope you know this was definetly the right move."  
