2d6f7827cb5678cdd13345bbc4820645eaf5972a
Max V. <mountainwater101z@gmail.com>
2026-07-30 20:03:46 -0500

    Add basic inventory system with item checks

--- yellow_bird.py
+++ yellow_bird.py
@@ -1,1 +1,17 @@
+#INVENTORY SYSTEM 
 
+inventory = []
+
+inventory.append("Silver Key")
+inventory.append("Old Letter")
+
+print("Inventory: ")
+for item in inventory: 
+    print("-", item)
+
+# PRACTICE CHECKING FOR ITEMS
+
+if "Silver Key" in inventory:
+  print("You unlock the door.")
+else: 
+    print("The door is locked.")
