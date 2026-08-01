5d684d9fb92d2f5a2e4c03017964225da6a2117a
Max V. <mountainwater101z@gmail.com>
2026-08-01 17:13:23 -0500

    Implement character creation in cherry_pie.py
    Added character creation section with background options.

--- cherry_pie.py
+++ cherry_pie.py
@@ -1,1 +1,16 @@
+# THIS IS JUST A PRACTICE SCENARIO SO I CAN LEARN A THING OR TWO
+
+print("xo" * 10)
+print("    THE LAST NIGHT: Z0M813 APOCALYPSE  ")
+
+#-------------------------------------------------------------
+# CHARACTER CREATION
+#-------------------------------------------------------------
+name = input("n\What is your character name ? ").strip() 
+
+print("\nChoose your background:")
+print("1. Soldier      - Srong in combat")
+print("2. Medic        - Better at healing and using the cure")
+print("3. Engineer     - Better at repairing and defending")
+print("4. Survivor     - Balanced skills and extra supplies")
 
